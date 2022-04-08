d6292201d227222aa18efaecc01e80bb0ff1c278
Leopoldo Manfio <leopoldo.manfio@iovlabs.org>
2022-04-08 02:09:34 -0300

    add tokens list constant

---
+++ src/constants/tokens/tokens.ts
@@ -0,0 +1,17 @@
+export const RBTC_TOKEN = {
+  token: "RBTC",
+  name: "Rsk BTC",
+  typeId: 4,
+  icon: "",
+  symbol: "rBTC",
+  address: "0x7c77704007C9996Ee591C516f7319828BA49d91E",
+};
+
+export const RKOVWBTC_TOKEN = {
+  token: "WBTC",
+  name: "Wrapped BTC",
+  typeId: 0,
+  icon: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599/logo.png",
+  symbol: "WBTC",
+  address: "0xd1b98b6607330172f1d991521145a22bce793277",
+};
