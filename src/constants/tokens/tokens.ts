a8166a84f0f4e7fe6546b63421dff7bbedc0b116
Leopoldo Manfio <leopoldo.manfio@iovlabs.org>
2022-04-12 02:41:23 -0300

    fix address

--- src/constants/tokens/tokens.ts
+++ src/constants/tokens/tokens.ts
@@ -12,5 +12,5 @@ export const RKOVWBTC_TOKEN = {
   typeId: 0,
   icon: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599/logo.png",
   symbol: "WBTC",
-  address: "0xd1b98b6607330172f1d991521145a22bce793277",
+  address: "0xb8aE2CB769255359190fBcE89d3aD38687da5e65",
 };
