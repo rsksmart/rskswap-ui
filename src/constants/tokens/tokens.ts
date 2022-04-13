31704148651ed8ee193f4580409fe3c5b515a160
Leopoldo Manfio <leopoldo.manfio@iovlabs.org>
2022-04-13 19:10:12 -0300

    add correct address and type

--- src/constants/tokens/tokens.ts
+++ src/constants/tokens/tokens.ts
@@ -7,7 +7,8 @@ export const RBTC_TOKEN: Token = {
   decimals: 18,
   icon: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599/logo.png",
   symbol: "rBTC",
-  address: '0x',
+  address: "0x48288D0e3079A03f6EC1846554CFc58C2696Aaee",
+  type: "NATIVE",
 };
 
 export const RKOVWBTC_TOKEN: Token = {
@@ -17,7 +18,8 @@ export const RKOVWBTC_TOKEN: Token = {
   decimals: 18,
   icon: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599/logo.png",
   symbol: "rKovWBTC",
-  address: '0xB8ae2CB769255359190Fbce89D3ad38687DA5E65',
+  address: "0xB8ae2CB769255359190Fbce89D3ad38687DA5E65",
+  type: "ERC20",
 };
 
 export const ALL_TOKENS: Token[] = [RBTC_TOKEN, RKOVWBTC_TOKEN];
