8dae5bb48b24b2a02a145c0f598b71f0da0cc489
augusto.chirico <augusto.chirico@iovlabs.org>
2022-05-02 14:23:21 +0200

    add ci

--- tests/test-setup.ts
+++ tests/test-setup.ts
@@ -2,9 +2,10 @@
 
 // mocking TextEncoder/TextDecoder
 const init = () => {
-    const { TextEncoder, TextDecoder } = require('util')
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { TextEncoder, TextDecoder } = require("util");
 
-    global.TextEncoder = TextEncoder
-    global.TextDecoder = TextDecoder
-}
-init()
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
+};
+init();
