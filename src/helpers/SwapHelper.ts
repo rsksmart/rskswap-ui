3cd87eefeba73a1b70843594d9dd09148b3e2b8c
Guilherme Soares <guilherme.soares@iovlabs.org>
2022-06-10 11:44:47 -0300

    Fixed lint

--- src/helpers/SwapHelper.ts
+++ src/helpers/SwapHelper.ts
@@ -3,7 +3,7 @@ import { MAX_SWAP_AMOUNT } from '@/constants/variables';
 export function getMaximumAllowed(userBalance: number, relayerBalance: number) {
     let maxSwap = MAX_SWAP_AMOUNT;
     
-    Math.min(userBalance, relayerBalance) > maxSwap ? maxSwap = maxSwap :
+    Math.min(userBalance, relayerBalance) > maxSwap ? null :
         userBalance > relayerBalance ? maxSwap = relayerBalance : maxSwap = userBalance;
     
     return maxSwap;
