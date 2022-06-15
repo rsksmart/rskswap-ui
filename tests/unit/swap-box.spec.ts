f43162977b3bc3ea7630f48876ae72795ad552fb
Guilherme Soares <guilherme.soares@iovlabs.org>
2022-06-15 16:55:29 -0300

    Added cypress

--- tests/unit/swap-box.spec.ts
+++ tests/unit/swap-box.spec.ts
@@ -1,33 +1,12 @@
-import { shallowMount, VueWrapper } from "@vue/test-utils";
 import SwapBox from "@/components/swap/SwapBox.vue";
-import { createStore } from "vuex";
-import { session } from "@/store/session";
-
-const store = createStore({
-  mutations: {},
-  actions: {},
-  modules: {
-    session,
-  },
-});
+import { mount } from "@cypress/vue";
 
 describe("SwapBox.vue", () => {
-  let wrapper: VueWrapper<any>;
   beforeEach(() => {
-    wrapper = shallowMount(SwapBox, {
-      global: {
-        plugins: [store],
-      },
-    });
+    mount(SwapBox);
   });
 
   it("Should render the whole screen", async () => {
-    expect(wrapper.find("box-body")).toBeDefined();
-    expect(wrapper.find("currency-input")).toBeDefined();
-    expect(wrapper.find("input-note")).toBeDefined();
-    expect(wrapper.find("addressBox")).toBeDefined();
-    expect(wrapper.find("clipboard-icon")).toBeDefined();
-    expect(wrapper.find("tooltip-balance")).toBeDefined();
-    expect(wrapper.find("btn btn-primary py-3 rounded w-50")).toBeDefined();
+    cy.find('#swapInput').should('be.visible');
   });
 });
