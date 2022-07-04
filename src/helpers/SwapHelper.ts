7ce8a8b9e03c1b29ffc898350da6d86742393947
Guilherme Soares <guilherme.soares@iovlabs.org>
2022-07-04 11:39:28 -0300

    Fixed the getMaxAlloed to include the swap balance

--- src/helpers/SwapHelper.ts
+++ src/helpers/SwapHelper.ts
@@ -1,10 +1,10 @@
 import { MAX_SWAP_AMOUNT } from '@/constants/variables';
 
-export function getMaximumAllowed(userBalance: number, relayerBalance: number) {
+export function getMaximumAllowed(userBalance: number, relayerBalance: number, swapBalance: number) {
     let maxSwap = MAX_SWAP_AMOUNT;
 
-    if (Math.min(userBalance, relayerBalance) <= maxSwap) {
-        maxSwap = userBalance > relayerBalance ? relayerBalance : userBalance;
+    if (Math.min(userBalance, relayerBalance, swapBalance) <= maxSwap) {
+        maxSwap = Math.min(userBalance, relayerBalance, swapBalance);
     }
     
     return maxSwap;
