4d963bcab3f328a874ae089bc14193014d4ab335
manfioLP <leopoldo.manfio@iovlabs.org>
2022-08-03 11:17:40 -0300

    adjust test value

--- tests/unit/swap-box.spec.ts
+++ tests/unit/swap-box.spec.ts
@@ -71,7 +71,7 @@ describe("SwapBox.vue", () => {
     await wrapper.vm.$nextTick();
 
     expect(spyGetGasCost).toBeCalledWith(GAS_AVG);
-    expect(wrapper.vm.swapTo.value).toEqual("0.099997800000");
+    expect(wrapper.vm.swapTo.value).toEqual("0.0999978");
   });
 
   it("Should select the correct address type", () => {
